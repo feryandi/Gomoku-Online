a1974953158c33b9c240e694b202bb5860d952be
NithoAlif <nithoalif@yahoo.com>
2015-12-04 16:40:52 +0700

    fix multiple message bug

--- client/Gomoku-Online-Client/client.cpp
+++ client/Gomoku-Online-Client/client.cpp
@@ -63,35 +63,44 @@ void client::readMessage()
 	QByteArray message = socket->readAll();
 
 	qDebug() << "reading...";
-	qDebug() << message;
+	//qDebug() << message;
 
-	QJsonDocument json_document = QJsonDocument::fromJson(message);
+	QList<QByteArray> message_list= message.split('\n');
+	qDebug() << message_list;
+
+	QJsonDocument json_document;
 	QJsonObject json_object;
-	json_object = json_document.object();
-	QJsonValue type = json_object.value("type");
+	QJsonValue type;
+	for (int i=0; i<message_list.size(); i++){
+		json_document = QJsonDocument::fromJson(message_list.at(i));
+		json_object = json_document.object();
+		type = json_object.value("type");
 
-	if (type == "login"){
-		player_id = json_object.value("id").toInt();
-		emit on_login();
-	} else if (type == "response"){
-        if (json_object.value("object") == "rooms"){
-			rooms = json_object.value("data").toArray();
-			emit on_refresh_rooms(rooms);
-		} else if (json_object.value("object") == "players"){
-			players = json_object.value("data").toArray();
-			emit on_refresh_players(players);
+		if (type == "login"){
+			player_id = json_object.value("id").toInt();
+			emit on_login();
+		} else if (type == "response"){
+			if (json_object.value("object") == "rooms"){
+				rooms = json_object.value("data").toArray();
+				emit on_refresh_rooms(rooms);
+			} else if (json_object.value("object") == "players"){
+				players = json_object.value("data").toArray();
+				emit on_refresh_players(players);
+			}
+		} else if (type == "newroom"){
+			emit on_create_room(json_object.value("rid").toInt());
+		} else if (type == "join"){
+			emit on_join(json_object.value("rid").toInt());
+		} else if (type == "closegame") {
+			emit on_close_game();
+		} else if (type == "startgame") {
+			emit on_start_game();
+		} else if (type == "play") {
+			emit on_update_game(json_object);
+		} else if (type == "win") {
+			emit on_game_over(json_object);
 		}
-	} else if (type == "newroom"){
-		emit on_create_room(json_object.value("rid").toInt());
-	} else if (type == "join"){
-		emit on_join(json_object.value("rid").toInt());
-	} else if (type == "closegame") {
-		emit on_close_game();
-	} else if (type == "startgame") {
-		emit on_start_game();
-	} else if (type == "play") {
-		emit on_update_game(json_object);
-	} else if (type == "win") {
-		emit on_game_over(json_object);
 	}
+
+
 }
--- server/server_structure.py
+++ server/server_structure.py
@@ -25,13 +25,13 @@ class GameServer:
 		msg = json.dumps(message,separators=(',',':'))
 		for player in self.players:
 			if ( ( player != "" ) and ( player.getRoomID() == rid ) ):
-				player.getIPort().send(msg + "\r\n")
+				player.getIPort().send(msg + "\n")
 
 	def broadcastByPID (self, pid, message):
 		msg = json.dumps(message,separators=(',',':'))
 		player = self.players[pid]
 		if player != "":
-			player.getIPort().send(msg + "\r\n")
+			player.getIPort().send(msg + "\n")
 
 
 	def newPlayer (self, name, iport):
@@ -101,7 +101,7 @@ class MessageServer:
 					# data += msg
 
 				msg = clientsocket.recv(4096)
-				msg = msg.split("\r\n", 1)
+				msg = msg.split("\n", 1)
 				data = msg[0]
 				if not data:
 					break
@@ -236,7 +236,7 @@ class MessageServer:
 
 	def sendResponse (self, clientsocket, msg):
 		print msg
-		clientsocket.send(msg +"\r\n")
+		clientsocket.send(msg +"\n")
 
 
 	def __del__ (self):
